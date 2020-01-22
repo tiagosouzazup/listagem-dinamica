b5a66616e163d199d647525a47017160c2b1eac6
Jonatas Oliveira <Jonatas.sousa@zup.com.br>
2020-01-22 13:45:32 -0300

    alterando o button da aplicacao

--- src/components/ListaNomes/index.js
+++ src/components/ListaNomes/index.js
@@ -1,26 +1,23 @@
-import React, {useState} from  'react';
-
+import React, { useState } from 'react';
 import Lista from '../Lista';
-
 import Tarefa from '../Tarefa';
-
 
 const ListaNomes = () => {
     const [tarefas, setTarefas] = useState([]);
 
     function addNovaTarefa(tarefa) {
         const nomes = Array.from(tarefas);
-        nomes.push({id: tarefas.length, value: tarefa});
+        nomes.push({ id: tarefas.length, value: tarefa });
         setTarefas(nomes);
     }
     return (
         <div>
             <Tarefa onSubmit={addNovaTarefa} />
-            {tarefas.map(({id, value}, index) => (
-              <Lista
-              key={id}
-              value={value}
-              /> 
+            {tarefas.map(({ id, value }, index) => (
+                <Lista
+                    key={id}
+                    value={value}
+                />
             ))}
         </div>
     )
